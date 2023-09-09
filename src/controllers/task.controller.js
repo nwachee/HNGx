b1ff6280642fd6059def866db14bf3ffd6c42c03
Nwachee <fuzzy245.in@gmail.com>
2023-09-09 01:12:51 +0100

    Fix: minor changes

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -4,24 +4,22 @@ export const getTask = async(req, res, next) => {
     try {
     const slackName = req.query.slack_name;
      const track = req.query.track;
-
-    const utcTime = moment().utc().format('YYYY-MM-DDTHH:mm:ssZ');
-
+    // const utcTime = moment().utc().format('YYYY-MM-DDTHH:mm:ssZ');
+  const utcTime = moment().utc();
+  const utcOffset = moment().utcOffset();
+  const currentUTCTime = utcTime.add(utcOffset, 'minutes');
   const githubFileUrl = `https://github.com/nwachee/HNGx/blob/main/src/app.js`;
   const githubRepoUrl = `https://github.com/nwachee/HNGx.git`;
-
   const currentDay = moment().format('dddd');
-
-  const response = {
-    slack_name: slackName,
-    current_day: currentDay,
-    utc_time: utcTime,
-    track: track,
-    github_file_url: githubFileUrl,
-    github_repo_url: githubRepoUrl,
-    status_code: 200
-  };
-      res.status(201).send(response);
+      res.status(200).json({ 
+        slack_name: slackName,
+        current_day: currentDay,
+         utc_time: currentUTCTime,
+        track: track,
+        github_file_url: githubFileUrl,
+        github_repo_url: githubRepoUrl,
+        status_code: 200
+      });
     } catch (error) {
         next(error)
     }
